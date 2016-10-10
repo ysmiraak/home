a34cdb0397cb40c4b8bfb08c52d4ff3a198a7078
Ysmiraak <ysmiraak@gmail.com>
2016-10-10 03:00:31 +0200

    warn-on-refl for lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,4 +4,5 @@
                   [lein-kibit "0.1.2"]
                   [jonase/eastwood "0.2.3"]]
         :dependencies [[com.cemerick/pomegranate "0.3.1"]]
-        :repl-options {:init (set! *print-length* 11)}}}
+        :repl-options {:init (do (set! *print-length* 11)
+                                 (set! *warn-on-reflection* true))}}}
