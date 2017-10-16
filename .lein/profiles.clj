9f6fcf3a1ed32a8f16cc49b489d836b425847aa5
ysmiraak <ysmiraak@gmail.com>
2017-10-16 11:49:15 +0200

    update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,11 @@
-{:user {:plugins [[lein-ancient "0.6.10"]
+{:user {:plugins [[lein-ancient "0.6.12"]
                   [lein-pprint "1.1.2"]
-                  [lein-codox "0.10.0"]
-                  [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
-                  [jonase/eastwood "0.2.4"]]}
- :repl {:dependencies [^:displace [org.clojure/clojure "1.9.0-alpha17"]
-                       [ysmiraak "2017.09.11"]
-                       [com.cemerick/pomegranate "0.3.1"]]
+                  [lein-codox "0.10.3"]
+                  [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
+                  [jonase/eastwood "0.2.5"]]}
+ :repl {:dependencies [^:displace [org.clojure/clojure "1.9.0-beta2"]
+                       [ysmiraak "2017.10.16"]
+                       [com.cemerick/pomegranate "0.4.0"]]
         :repl-options {:init-ns ysmiraak}
         :global-vars {*warn-on-reflection* true
                       *print-length* 11}
