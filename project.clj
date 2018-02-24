7bd5931bf40b2e511f0208f976e1a4cf50709ac4
ysmiraak <ysmiraak@gmail.com>
2018-02-25 03:03:24 +0800

    update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ysmiraak "2017.12.10"
+(defproject ysmiraak "2018.02.25"
   :description "a library of stolen ideas."
   :url "https://github.com/ysmiraak/ysmiraak"
   :license {:name "Eclipse Public License"
@@ -6,15 +6,15 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.9.0"]
                  [swiss-arrows "1.0.0"]
-                 [uncomplicate/fluokitten "0.6.0"]
-                 [uncomplicate/neanderthal "0.17.1" :exclusions [org.clojure/tools.analyzer.jvm]]
-                 [com.rpl/specter "1.0.5"]
+                 [uncomplicate/fluokitten "0.6.1"]
+                 [uncomplicate/neanderthal "0.18.0" :exclusions [org.clojure/tools.analyzer.jvm]]
+                 [com.rpl/specter "1.1.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/core.logic "0.8.11"]
-                 [net.mikera/core.matrix "0.61.0"]
+                 [net.mikera/core.matrix "0.62.0"]
                  [net.mikera/vectorz-clj "0.47.0"]
                  [org.clojure/data.csv "0.1.4"]
-                 [org.clojure/core.async "0.3.465" :exclusions [org.clojure/tools.analyzer.jvm]]
+                 [org.clojure/core.async "0.4.474" :exclusions [org.clojure/tools.analyzer.jvm]]
                  [criterium "0.4.4"]]
   :repl-options {:init-ns ysmiraak}
   :codox {:output-path "docs"
