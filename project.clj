d26b16d9e98cfaee187dcd2a0db07b07f580ee1b
ysmiraak <ysmiraak@gmail.com>
2018-05-02 22:03:26 +0200

    upgrade deps and docs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ysmiraak "2018.02.25"
+(defproject ysmiraak "2018.05.02"
   :description "a library of stolen ideas."
   :url "https://github.com/ysmiraak/ysmiraak"
   :license {:name "Eclipse Public License"
@@ -7,8 +7,8 @@
                  [org.clojure/test.check "0.9.0"]
                  [swiss-arrows "1.0.0"]
                  [uncomplicate/fluokitten "0.6.1"]
-                 [uncomplicate/neanderthal "0.18.0" :exclusions [org.clojure/tools.analyzer.jvm]]
-                 [com.rpl/specter "1.1.0"]
+                 [uncomplicate/neanderthal "0.19.0" :exclusions [org.clojure/tools.analyzer.jvm]]
+                 [com.rpl/specter "1.1.1"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/core.logic "0.8.11"]
                  [net.mikera/core.matrix "0.62.0"]
