377d7fd759aea24fdba715c86776ac28585b720a
ysmiraak <ysmiraak@gmail.com>
2018-06-22 16:45:08 +0200

    clean up bash and fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,9 @@
+# fish for mac os
+
 set -g -x EMAIL ysmiraak@gmail.com
 set -g -x ALTERNATE_EDITOR emacs
 set -g -x EDITOR emacsclient
 set -g -x VISUAL emacsclient
 
-set -g -x LANG en_US.UTF-8
-set -g -x LC_ALL en_US.UTF-8
-set -g -x PATH /usr/local/bin /usr/local/sbin ~/.cabal/bin ~/.cargo/bin $PATH
-
-set -g -x TERM xterm-256color
-set -g -x ARCHFLAGS "-arch x86_64"
+set -g -x PATH /usr/local/bin /usr/local/sbin $PATH
+set -g -x PATH ~/.cabal/bin ~/.cargo/bin $PATH
