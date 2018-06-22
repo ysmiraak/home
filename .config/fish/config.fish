8d094b4e76e27c07f726908513860014ef0e664b
ysmiraak <ysmiraak@gmail.com>
2018-06-22 17:05:01 +0200

    minor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,4 +6,4 @@ set -g -x EDITOR emacsclient
 set -g -x VISUAL emacsclient
 
 set -g -x PATH /usr/local/bin /usr/local/sbin $PATH
-set -g -x PATH ~/.cabal/bin ~/.cargo/bin $PATH
+set -g -x PATH $PATH ~/.cabal/bin ~/.cargo/bin
