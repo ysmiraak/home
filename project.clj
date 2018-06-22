7ba9c90e95c15eb4a1c6788c1655d51c7391a00d
ysmiraak <ysmiraak@gmail.com>
2018-06-22 18:55:42 +0200

    update clojure

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject ysmiraak "2018.05.02"
+(defproject ysmiraak "2018.06.22"
   :description "a library of stolen ideas."
   :url "https://github.com/ysmiraak/ysmiraak"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/test.check "0.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0-alpha4"]
+                 [org.clojure/test.check "0.10.0-alpha3"]
                  [swiss-arrows "1.0.0"]
                  [uncomplicate/fluokitten "0.6.1"]
                  [uncomplicate/neanderthal "0.19.0" :exclusions [org.clojure/tools.analyzer.jvm]]
                  [com.rpl/specter "1.1.1"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
                  [org.clojure/core.logic "0.8.11"]
                  [net.mikera/core.matrix "0.62.0"]
                  [net.mikera/vectorz-clj "0.47.0"]
