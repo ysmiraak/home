e4c98f6fa88ca8e90420a2ec3ec3e77635a86dd1
ysmiraak <ysmiraak@gmail.com>
2018-10-28 03:14:43 +0100

    xmonad

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -1,19 +1,21 @@
-Config { font = "-*-Fixed-Bold-R-Normal-*-13-*-*-*-*-*-*-*"
-       , bgColor = "black"
-       , fgColor = "grey"
-       , position = TopW L 99
-       , lowerOnStart = True
-       , commands = [ Run Cpu [ "-t" , "cpu <total>%"
-                              , "-L" , "3"
-                              , "-H" , "50"
-                              , "--normal" , "green"
-                              , "--high" , "red"
-                              ] 10
-                    , Run Memory ["-t","mem <usedratio>%"] 10
-                    , Run Date "%a %b %_d %H:%M" "date" 10
-                    , Run StdinReader
-                    ]
-       , sepChar = "%"
-       , alignSep = "}{"
-       , template = "%StdinReader% %cpu% %memory% }{ %date%"
-       }
+Config
+  { template = "%StdinReader% }{ cpu %cpu% mem %memory%    %date%"
+  , commands =
+      [ Run StdinReader
+      , Run Cpu
+        [ "--template" , "<total>%"
+        , "--Low"      , "20"
+        , "--High"     , "50"
+        , "--normal"   , "green"
+        , "--high"     , "red"
+        ] 10
+      , Run Memory
+        [ "--template" , "<usedratio>%"
+        , "--Low"      , "20"
+        , "--High"     , "50"
+        , "--normal"   , "green"
+        , "--high"     , "red"
+        ] 10
+      , Run Date "%a %b %_d %H:%M" "date" 10
+      ]
+  }
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,24 +1,33 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
+import qualified System.IO as IO
+import qualified XMonad as XMonad
+import qualified XMonad.Config.Desktop as Desktop
+import qualified XMonad.Hooks.DynamicLog as DynamicLog
+import qualified XMonad.Hooks.ManageDocks as ManageDocks
+import qualified XMonad.Layout.Tabbed as Tabbed
+import qualified XMonad.Util.EZConfig as EZConfig
+import qualified XMonad.Util.Run as Run
 
 main = do
-  xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
-  xmonad $ def
-    { modMask = mod4Mask
+  xmproc <- Run.spawnPipe "xmobar ~/.xmonad/xmobar.hs"
+  XMonad.xmonad $ Desktop.desktopConfig
+    { XMonad.modMask = XMonad.mod4Mask -- use the windows key as mod
+    , XMonad.borderWidth = 0 -- no border
+    , XMonad.layoutHook =
+        Tabbed.simpleTabbed     -- add a fullscreen tabbed layout
+        XMonad.||| XMonad.layoutHook Desktop.desktopConfig
+    , XMonad.logHook =          -- stdin for xmobar
+        DynamicLog.dynamicLogWithPP DynamicLog.xmobarPP
+        { DynamicLog.ppOutput = IO.hPutStrLn xmproc
+        , DynamicLog.ppOrder = \(ws:l:t:_) -> [ws,t] -- only show workspaces and title
+        , DynamicLog.ppSep = "  "                    -- separated by two spaces
+        , DynamicLog.ppCurrent = DynamicLog.wrap "[" "]" -- wrap current workspace
+        , DynamicLog.ppTitle = id -- vanilla title
+        }
+    } `EZConfig.additionalKeysP`
+    [ ("M-S-t", XMonad.sendMessage ManageDocks.ToggleStruts)
+    ]
 
-    , borderWidth = 0
-
-    , handleEventHook = handleEventHook def <+> docksEventHook <+> fullscreenEventHook
-
-    , layoutHook = avoidStruts  $  layoutHook def
-    , manageHook = manageDocks <+> manageHook def
-    , logHook = dynamicLogWithPP xmobarPP
-                { ppOutput = hPutStrLn xmproc
-                , ppOrder = \(ws:l:t:_) -> [ws]
-                }
-    }
+-- todo auto workspaces
+-- 9 emacs firefox
+-- 8 vlc
+-- 1 xterm
