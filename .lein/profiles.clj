f5979c7d69c76383edd82943b98c15556c18e589
ysmiraak <ysmiraak@gmail.com>
2020-05-18 13:25:35 +0200

    repl plugins in lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,8 @@
 {:user {:plugins [[lein-ancient "0.6.15"]
                   [lein-pprint "1.3.2"]
                   [lein-codox "0.10.7"]]}
- :repl {:dependencies [[com.cemerick/pomegranate "1.1.0"]]
+ :repl {:plugins [[cider/cider-nrepl "0.25.0-SNAPSHOT"]
+                  [refactor-nrepl "2.5.0"]]
+        :dependencies [[com.cemerick/pomegranate "1.1.0"]]
         :global-vars {*warn-on-reflection* true *print-length* 11}
         :jvm-opts ["-Xmx2g"]}}
